586a6bc6a17d8b65b76ddeb8019cbd917e87626c
Liffon <liffon@gmail.com>
2020-02-05 21:28:30 +0100

    More music, more prototyping

--- keyboards/handwired/liffon/config.h
+++ keyboards/handwired/liffon/config.h
@@ -89,6 +89,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
+#define USB_MAX_POWER_CONSUMPTION 100
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
